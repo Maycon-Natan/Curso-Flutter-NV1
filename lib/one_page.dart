066e1a14179934e2ee81e6353e1871912d0f9803
Maycon-Natan <mayconnatan7@gmail.com>
2021-11-03 21:47:09 -0300

    consume api

--- lib/one_page.dart
+++ lib/one_page.dart
@@ -1,6 +1,8 @@
-import 'dart:math' show Random;
+import 'dart:convert';
+import 'dart:io';
 import 'package:elevatedbutton/widgets/custom_button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class OnePage extends StatefulWidget {
   const OnePage({Key? key}) : super(key: key);
@@ -10,39 +12,82 @@ class OnePage extends StatefulWidget {
 }
 
 class _OnePageState extends State<OnePage> {
-  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
 
-  random() async {
-    for (int i = 0; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      valorAleatorio.value = Random().nextInt(1000);
+  callAPI() async {
+    var client = HttpClient();
+    try {
+      inLoader.value = true;
+      var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+      var response = await http.get(url);
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+      await Future.delayed(const Duration(seconds: 2));
+    } finally {
+      client.close();
+      inLoader.value = false;
     }
   }
+
+  // ----Metodo Random----
+  // random() async {
+  //   for (int i = 0; i < 10; i++) {
+  //     await Future.delayed(const Duration(seconds: 1));
+  //     valorAleatorio.value = Random().nextInt(1000);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder(
-              valueListenable: valorAleatorio,
-              builder: (_, value, __) => Text(
-                "Valor é: $value",
-                style: const TextStyle(fontSize: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: Listenable.merge([posts, inLoader]),
+                builder: (_, __) => inLoader.value
+                    ? const CircularProgressIndicator()
+                    : ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: posts.value.length,
+                        itemBuilder: (_, index) => ListTile(
+                          title: Text(posts.value[index].title),
+                        ),
+                      ),
               ),
-            ),
-            const SizedBox(height: 10),
-            CustomButtonWidget(
-              disable: false,
-              onPressed: () => random(),
-              title: 'Custom BTN',
-              titleSize: 18,
-            ),
-          ],
+              const SizedBox(height: 10),
+              CustomButtonWidget(
+                disable: false,
+                onPressed: () => callAPI(),
+                title: 'Custom BTN',
+                titleSize: 18,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+    return Post(json['userId'], json['id'], json['title'], json['body']);
+  }
+
+  @override
+  String toString() {
+    return 'id: $id';
   }
 }
